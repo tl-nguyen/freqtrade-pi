10b089bdb9decc885110e7e7e264e120850edacf
TL Nguyen <tung.lam.nguyen@mentormate.com>
2019-04-01 16:29:59 +0300

    Bug fix for checking installed docker; Remove trailing \r when installing from Windows machine

--- remote_install.sh
+++ remote_install.sh
@@ -6,8 +6,10 @@ if [ -z "$USERNAME_AT_SERVER" ]; then
  echo "Usage: bash remote_install.sh [username@server_ip]"
  echo "Example: bash remote_install.sh pi@192.168.1.10"
 else
- if ! [ -x "$(command -v docker)" ]; then
+ DOCKER_CHECK=$(ssh $USERNAME_AT_SERVER "command -v docker")
+ if [ -z "$DOCKER_CHECK" ]; then
   ssh $USERNAME_AT_SERVER "sudo curl -sSL https://get.docker.com | sh && sudo docker pull tlnguyen/freqtrade.pi && mkdir ~/freqtrade"
  fi
+ sed -i 's/\r$//' ./freqtrade/start.sh && sed -i 's/\r$//' ./freqtrade/stop.sh
  scp -r ./freqtrade/* $USERNAME_AT_SERVER:~/freqtrade
 fi
